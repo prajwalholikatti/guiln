58a6a586e7840c2e4247057b8ca2ab165470fb0d
prajwalholikatti <grayarea012@gmail.com>
2026-06-29 22:19:49 +0530

    indentation2

--- VALUES.py
+++ VALUES.py
@@ -12,10 +12,10 @@ def fetch_info(ticker):
 def get_data(company1,company2):
   
  
-    try:
+   try:
         info = fetch_info(f"{company1}.NS")
         info1 = fetch_info(f"{company2}.NS")
-    except Exception:
+   except Exception:
         return "Yahoo Finance rate limit exceeded. Try again later."
 
    metrics= [
